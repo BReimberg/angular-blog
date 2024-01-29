e396220dc94de277eb19b035db98b7be329b4a3a
Brenda Reimberg <brendasrsouza@gmail.com>
2024-01-29 11:12:41 -0300

    Adicionando Footer ao blog

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -3,12 +3,13 @@ export const dataFake = [
     "id":"0",
     "photo":"https://miro.medium.com/v2/resize:fit:1358/0*KNqK1IFDqENcyGXS",
     "title": "Nova versão do Angular promete revoluções",
-    "description": `
+     "description": `
           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           Donec luctus lacinia libero ac fermentum. Nam accumsan maximus
           sapien, nec convallis leo bibendum a. Vestibulum ante ipsum
           primis in faucibus orci luctus et ultrices posuere cubilia
           curae; Maecenas malesuada gravida libero eget elementum.
+
           In hac habitasse platea dictumst. Nulla eget accumsan lacus.
           Nulla ornare libero ut libero blandit, eu fringilla odio efficitur.
           Vivamus gravida, sem ut congue placerat, risus diam dignissim est,
@@ -19,6 +20,7 @@ export const dataFake = [
           ipsum primis in faucibus. Orci varius natoque penatibus et magnis
           dis parturient montes, nascetur ridiculus mus. Donec fringilla, quam sit
           amet varius vehicula, urna turpis ornare nisi, vel auctor ex felis quis neque.
+
           Quisque vel tristique ante, vel pharetra est. Donec neque augue, egestas ac
           tortor a, viverra faucibus turpis. Suspendisse potenti. Proin hendrerit commodo
           laoreet. Maecenas et pretium justo, id viverra erat. Etiam ac suscipit odio,
@@ -30,7 +32,6 @@ export const dataFake = [
           nulla eros quis urna. Integer feugiat mi a metus suscipit pharetra.
           Mauris quis nibh eu odio placerat fringilla quis sit amet nisi.
           Nam semper nisi ac luctus facilisis.
-
     `
   },
 
@@ -48,6 +49,7 @@ export const dataFake = [
           Nulla ornare libero ut libero blandit, eu fringilla odio efficitur.
           Vivamus gravida, sem ut congue placerat, risus diam dignissim est,
           sed tristique elit diam nec purus.
+
           Donec in aliquam erat, ut scelerisque tellus.
           Sed pretium diam ex, vitae laoreet neque bibendum eget. Suspendisse et
           ante eget purus pulvinar dictum. Interdum et malesuada fames ac ante
@@ -58,6 +60,7 @@ export const dataFake = [
           tortor a, viverra faucibus turpis. Suspendisse potenti. Proin hendrerit commodo
           laoreet. Maecenas et pretium justo, id viverra erat. Etiam ac suscipit odio,
           vel placerat turpis.
+
           Sed semper ultricies rutrum. Nulla facilisi.
           Aenean vel urna mollis, scelerisque mauris dapibus, semper leo. Donec magna
           libero, blandit nec volutpat vitae, fringilla sed arcu. Vestibulum pulvinar,
@@ -68,6 +71,7 @@ export const dataFake = [
           congue mauris. Nulla pellentesque augue eu laoreet luctus. Nam a vulputate
           purus. Duis mattis elit sit amet tincidunt sollicitudin. Praesent ac risus
           velit.
+
           Aliquam quis lorem accumsan, feugiat dui nec, tempor est.
           Suspendisse pretium nibh eget iaculis hendrerit. Proin sit amet tempor justo.
           Nulla facilisi. Sed quis dapibus justo. Duis congue pulvinar tortor,
@@ -79,8 +83,8 @@ export const dataFake = [
           maximus sapien cursus nec. Donec eget sem laoreet, luctus leo sit amet,
           dictum ipsum. Maecenas lobortis, tortor ut mattis sollicitudin, dolor
           lacus sollicitudin enim, scelerisque venenatis diam enim a sem. Nulla id
-          nulla cursus, lacinia sem eu, tempor metus.
-          In eu imperdiet ipsum.
+          nulla cursus, lacinia sem eu, tempor metus.In eu imperdiet ipsum.
+
           Mauris tempus ipsum et condimentum malesuada. In lacinia urna gravida quam
           tincidunt, at porttitor velit lobortis. Praesent non eros purus. Etiam
           condimentum, ligula ut varius viverra, sem mi egestas ipsum, a mattis magna
@@ -177,5 +181,217 @@ export const dataFake = [
         congue mauris. Nulla pellentesque augue eu laoreet luctus. Nam a vulputate
         purus.
     `
+  },
+
+  {
+    "id":"4",
+    "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5W60yulwGKbqtaTwRtEnrRYr2M1TXtQzdwA&usqp=CAU",
+    "title": "Framework de código aberto e front-end baseado em TypeScript",
+    "description": `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Donec luctus lacinia libero ac fermentum. Nam accumsan maximus
+        sapien, nec convallis leo bibendum a. Vestibulum ante ipsum
+        primis in faucibus orci luctus et ultrices posuere cubilia
+        curae; Maecenas malesuada gravida libero eget elementum.
+        In hac habitasse platea dictumst. Nulla eget accumsan lacus.
+        Nulla ornare libero ut libero blandit, eu fringilla odio efficitur.
+        Vivamus gravida, sem ut congue placerat, risus diam dignissim est,
+        sed tristique elit diam nec purus.
+
+        Donec in aliquam erat, ut scelerisque tellus.
+        Sed pretium diam ex, vitae laoreet neque bibendum eget. Suspendisse et
+        ante eget purus pulvinar dictum. Interdum et malesuada fames ac ante
+        ipsum primis in faucibus. Orci varius natoque penatibus et magnis
+        dis parturient montes, nascetur ridiculus mus. Donec fringilla, quam sit
+        amet varius vehicula, urna turpis ornare nisi, vel auctor ex felis quis neque.
+        Quisque vel tristique ante, vel pharetra est. Donec neque augue, egestas ac
+        tortor a, viverra faucibus turpis. Suspendisse potenti. Proin hendrerit commodo
+        laoreet. Maecenas et pretium justo, id viverra erat. Etiam ac suscipit odio,
+        vel placerat turpis.
+
+        Sed semper ultricies rutrum. Nulla facilisi.
+        Aenean vel urna mollis, scelerisque mauris dapibus, semper leo. Donec magna
+        libero, blandit nec volutpat vitae, fringilla sed arcu. Vestibulum pulvinar,
+        tellus ac lobortis vehicula, magna quam lobortis velit, sit amet tincidunt
+        nulla eros quis urna. Integer feugiat mi a metus suscipit pharetra.
+        Mauris quis nibh eu odio placerat fringilla quis sit amet nisi.
+        Nam semper nisi ac luctus facilisis. Donec et elementum tellus, vitae
+        congue mauris. Nulla pellentesque augue eu laoreet luctus. Nam a vulputate
+        purus.
+    `
+  },
+
+  {
+    "id":"5",
+    "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnnGJFVh0fs9gvWTV_7K5erTxtur4OXft0Pw&usqp=CAU",
+    "title": "Todas as versões do Angular e suas atualizações",
+    "description": `
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Donec luctus lacinia libero ac fermentum. Nam accumsan maximus
+          sapien, nec convallis leo bibendum a. Vestibulum ante ipsum
+          primis in faucibus orci luctus et ultrices posuere cubilia
+          curae; Maecenas malesuada gravida libero eget elementum.
+          In hac habitasse platea dictumst. Nulla eget accumsan lacus.
+          Nulla ornare libero ut libero blandit, eu fringilla odio efficitur.
+          Vivamus gravida, sem ut congue placerat, risus diam dignissim est,
+          sed tristique elit diam nec purus.
+          Donec in aliquam erat, ut scelerisque tellus.
+          Sed pretium diam ex, vitae laoreet neque bibendum eget. Suspendisse et
+          ante eget purus pulvinar dictum. Interdum et malesuada fames ac ante
+          ipsum primis in faucibus. Orci varius natoque penatibus et magnis
+          dis parturient montes, nascetur ridiculus mus. Donec fringilla, quam sit
+          amet varius vehicula, urna turpis ornare nisi, vel auctor ex felis quis neque.
+          Quisque vel tristique ante, vel pharetra est. Donec neque augue, egestas ac
+          tortor a, viverra faucibus turpis. Suspendisse potenti. Proin hendrerit commodo
+          laoreet. Maecenas et pretium justo, id viverra erat. Etiam ac suscipit odio,
+          vel placerat turpis.
+          Sed semper ultricies rutrum. Nulla facilisi.
+          Aenean vel urna mollis, scelerisque mauris dapibus, semper leo. Donec magna
+          libero, blandit nec volutpat vitae, fringilla sed arcu. Vestibulum pulvinar,
+          tellus ac lobortis vehicula, magna quam lobortis velit, sit amet tincidunt
+          nulla eros quis urna. Integer feugiat mi a metus suscipit pharetra.
+          Mauris quis nibh eu odio placerat fringilla quis sit amet nisi.
+          Nam semper nisi ac luctus facilisis. Donec et elementum tellus, vitae
+          congue mauris. Nulla pellentesque augue eu laoreet luctus. Nam a vulputate
+          purus. Duis mattis elit sit amet tincidunt sollicitudin. Praesent ac risus
+          velit.
+          Aliquam quis lorem accumsan, feugiat dui nec, tempor est.
+          Suspendisse pretium nibh eget iaculis hendrerit. Proin sit amet tempor justo.
+          Nulla facilisi. Sed quis dapibus justo. Duis congue pulvinar tortor,
+          id porta tortor tristique sed. Mauris semper aliquet nulla, at sodales
+          risus auctor id. Proin bibendum odio nec ligula volutpat, id condimentum
+          ante tincidunt. Aliquam in velit tortor. Nulla suscipit facilisis imperdiet.
+          Vestibulum id rutrum erat. Ut nec blandit sapien, ut fringilla nisl.
+          Sed sed orci felis. Cras tincidunt ullamcorper velit, ullamcorper
+          maximus sapien cursus nec. Donec eget sem laoreet, luctus leo sit amet,
+          dictum ipsum. Maecenas lobortis, tortor ut mattis sollicitudin, dolor
+          lacus sollicitudin enim, scelerisque venenatis diam enim a sem. Nulla id
+          nulla cursus, lacinia sem eu, tempor metus.
+          In eu imperdiet ipsum.
+    `
+  },
+
+  {
+    "id":"6",
+    "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwvzlFIE8-Qy1zNcpTZyQXzu17Ndv37lH2VQ&usqp=CAU",
+    "title": "Semelhanças e diferenças entre Angular e Vue",
+    "description": `
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Donec luctus lacinia libero ac fermentum. Nam accumsan maximus
+          sapien, nec convallis leo bibendum a. Vestibulum ante ipsum
+          primis in faucibus orci luctus et ultrices posuere cubilia
+          curae; Maecenas malesuada gravida libero eget elementum.
+          In hac habitasse platea dictumst. Nulla eget accumsan lacus.
+          Nulla ornare libero ut libero blandit, eu fringilla odio efficitur.
+          Vivamus gravida, sem ut congue placerat, risus diam dignissim est,
+          sed tristique elit diam nec purus.
+
+          Donec in aliquam erat, ut scelerisque tellus.
+          Sed pretium diam ex, vitae laoreet neque bibendum eget. Suspendisse et
+          ante eget purus pulvinar dictum. Interdum et malesuada fames ac ante
+          ipsum primis in faucibus. Orci varius natoque penatibus et magnis
+          dis parturient montes, nascetur ridiculus mus. Donec fringilla, quam sit
+          amet varius vehicula, urna turpis ornare nisi, vel auctor ex felis quis neque.
+          Quisque vel tristique ante, vel pharetra est. Donec neque augue, egestas ac
+          tortor a, viverra faucibus turpis. Suspendisse potenti. Proin hendrerit commodo
+          laoreet. Maecenas et pretium justo, id viverra erat. Etiam ac suscipit odio,
+          vel placerat turpis.
+
+          Sed semper ultricies rutrum. Nulla facilisi.
+          Aenean vel urna mollis, scelerisque mauris dapibus, semper leo. Donec magna
+          libero, blandit nec volutpat vitae, fringilla sed arcu. Vestibulum pulvinar,
+          tellus ac lobortis vehicula, magna quam lobortis velit, sit amet tincidunt
+          nulla eros quis urna. Integer feugiat mi a metus suscipit pharetra.
+          Mauris quis nibh eu odio placerat fringilla quis sit amet nisi.
+          Nam semper nisi ac luctus facilisis. Donec et elementum tellus, vitae
+          congue mauris. Nulla pellentesque augue eu laoreet luctus. Nam a vulputate
+          purus. Duis mattis elit sit amet tincidunt sollicitudin. Praesent ac risus
+          velit.
+
+          Aliquam quis lorem accumsan, feugiat dui nec, tempor est.
+          Suspendisse pretium nibh eget iaculis hendrerit. Proin sit amet tempor justo.
+          Nulla facilisi. Sed quis dapibus justo. Duis congue pulvinar tortor,
+          id porta tortor tristique sed. Mauris semper aliquet nulla, at sodales
+          risus auctor id. Proin bibendum odio nec ligula volutpat, id condimentum
+          ante tincidunt. Aliquam in velit tortor. Nulla suscipit facilisis imperdiet.
+          Vestibulum id rutrum erat. Ut nec blandit sapien, ut fringilla nisl.
+          Sed sed orci felis. Cras tincidunt ullamcorper velit, ullamcorper
+          maximus sapien cursus nec. Donec eget sem laoreet, luctus leo sit amet,
+          dictum ipsum. Maecenas lobortis, tortor ut mattis sollicitudin, dolor
+          lacus sollicitudin enim, scelerisque venenatis diam enim a sem. Nulla id
+          nulla cursus, lacinia sem eu, tempor metus.In eu imperdiet ipsum.
+
+          Mauris tempus ipsum et condimentum malesuada. In lacinia urna gravida quam
+          tincidunt, at porttitor velit lobortis. Praesent non eros purus. Etiam
+          condimentum, ligula ut varius viverra, sem mi egestas ipsum, a mattis magna
+          odio in velit. Mauris malesuada cursus sem, id dapibus metus vestibulum eget.
+          In ullamcorper quam eu augue malesuada faucibus. Donec luctus, est dapibus
+          malesuada accumsan, nisi justo tincidunt libero, et eleifend magna elit a est.
+          Integer fermentum arcu at pharetra pellentesque. Praesent vitae suscipit lectus.
+          Sed ex turpis, consectetur ut erat in, volutpat molestie ligula. Duis facilisis
+          consequat neque sit amet viverra.
+      `
+  },
+
+  {
+    "id":"7",
+    "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiObA19fyF0XK71k4jmwuoNvGekL6gQ042InBNyXsarD8Gmzn7nE6GqqNASH9d49A9mJ0&usqp=CAU",
+    "title": "Como começar com Angular?",
+    "description": `
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Donec luctus lacinia libero ac fermentum. Nam accumsan maximus
+          sapien, nec convallis leo bibendum a. Vestibulum ante ipsum
+          primis in faucibus orci luctus et ultrices posuere cubilia
+          curae; Maecenas malesuada gravida libero eget elementum.
+          In hac habitasse platea dictumst. Nulla eget accumsan lacus.
+          Nulla ornare libero ut libero blandit, eu fringilla odio efficitur.
+          Vivamus gravida, sem ut congue placerat, risus diam dignissim est,
+          sed tristique elit diam nec purus.
+
+          Donec in aliquam erat, ut scelerisque tellus.
+          Sed pretium diam ex, vitae laoreet neque bibendum eget. Suspendisse et
+          ante eget purus pulvinar dictum. Interdum et malesuada fames ac ante
+          ipsum primis in faucibus. Orci varius natoque penatibus et magnis
+          dis parturient montes, nascetur ridiculus mus. Donec fringilla, quam sit
+          amet varius vehicula, urna turpis ornare nisi, vel auctor ex felis quis neque.
+          Quisque vel tristique ante, vel pharetra est. Donec neque augue, egestas ac
+          tortor a, viverra faucibus turpis. Suspendisse potenti. Proin hendrerit commodo
+          laoreet. Maecenas et pretium justo, id viverra erat. Etiam ac suscipit odio,
+          vel placerat turpis.
+
+          Sed semper ultricies rutrum. Nulla facilisi.
+          Aenean vel urna mollis, scelerisque mauris dapibus, semper leo. Donec magna
+          libero, blandit nec volutpat vitae, fringilla sed arcu. Vestibulum pulvinar,
+          tellus ac lobortis vehicula, magna quam lobortis velit, sit amet tincidunt
+          nulla eros quis urna. Integer feugiat mi a metus suscipit pharetra.
+          Mauris quis nibh eu odio placerat fringilla quis sit amet nisi.
+          Nam semper nisi ac luctus facilisis. Donec et elementum tellus, vitae
+          congue mauris. Nulla pellentesque augue eu laoreet luctus. Nam a vulputate
+          purus. Duis mattis elit sit amet tincidunt sollicitudin. Praesent ac risus
+          velit.
+
+          Aliquam quis lorem accumsan, feugiat dui nec, tempor est.
+          Suspendisse pretium nibh eget iaculis hendrerit. Proin sit amet tempor justo.
+          Nulla facilisi. Sed quis dapibus justo. Duis congue pulvinar tortor,
+          id porta tortor tristique sed. Mauris semper aliquet nulla, at sodales
+          risus auctor id. Proin bibendum odio nec ligula volutpat, id condimentum
+          ante tincidunt. Aliquam in velit tortor. Nulla suscipit facilisis imperdiet.
+          Vestibulum id rutrum erat. Ut nec blandit sapien, ut fringilla nisl.
+          Sed sed orci felis. Cras tincidunt ullamcorper velit, ullamcorper
+          maximus sapien cursus nec. Donec eget sem laoreet, luctus leo sit amet,
+          dictum ipsum. Maecenas lobortis, tortor ut mattis sollicitudin, dolor
+          lacus sollicitudin enim, scelerisque venenatis diam enim a sem. Nulla id
+          nulla cursus, lacinia sem eu, tempor metus.In eu imperdiet ipsum.
+
+          Mauris tempus ipsum et condimentum malesuada. In lacinia urna gravida quam
+          tincidunt, at porttitor velit lobortis. Praesent non eros purus. Etiam
+          condimentum, ligula ut varius viverra, sem mi egestas ipsum, a mattis magna
+          odio in velit. Mauris malesuada cursus sem, id dapibus metus vestibulum eget.
+          In ullamcorper quam eu augue malesuada faucibus. Donec luctus, est dapibus
+          malesuada accumsan, nisi justo tincidunt libero, et eleifend magna elit a est.
+          Integer fermentum arcu at pharetra pellentesque. Praesent vitae suscipit lectus.
+          Sed ex turpis, consectetur ut erat in, volutpat molestie ligula. Duis facilisis
+          consequat neque sit amet viverra.
+      `
   },
 ];
